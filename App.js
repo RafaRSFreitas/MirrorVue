757571de380bdc751c71c517f462e52d0308664c
RafaRSFreitas <rafael.r.freitas@hotmail.com>
2026-09-02 18:05:26 +0100

    completely full screen, locked to portrait

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
+import { SystemBars } from 'react-native-edge-to-edge';
+import * as ScreenOrientation from 'expo-screen-orientation';
 import CameraView from './src/components/CameraView';
 
 export default function App() {
+  useEffect(() => {
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+  }, []);
+  
   return (
     <View style={styles.container}>
       <CameraView />
